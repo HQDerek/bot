565fe08d532442492839633e3d070f48f2030074
Zachary <hello@zach.ie>
2018-07-10 20:46:38 +0100

    Fix pylint and pytest

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,6 +1,6 @@
 """ unit tests for the hqtrivia_bot.py """
 import pytest
-from mock import Mock, patch
+from mock import Mock
 import utils
 
 
@@ -16,16 +16,18 @@ def question_api_response():
     return question, answers
 
 
-@patch('utils.grequests.get')
-def test_find_answer_words_google(mock_grequests_get, question_api_response):  # pylint: disable=redefined-outer-name
+def test_find_answer_words_google(question_api_response):  # pylint: disable=redefined-outer-name
     """ testing basic behaviour in find_answer_words_google """
-    mock_response = Mock()
-    mock_grequests_get.return_value = mock_response
-    mock_response.url = "/"
-    mock_response.text = "Example Response"
+
+    mock_result = Mock()
+    mock_result.url = "/"
+    mock_result.text = "Example Response"
+
+    mock_session = Mock()
+    mock_session.get.return_value.result.return_value = mock_result
 
     question, answers = question_api_response
-    google_responses = utils.build_google_queries(question, answers)
+    google_responses = utils.build_google_queries(question, answers, mock_session)
 
     confidence = utils.find_answer_words_google(question, answers, {'A': 0, 'B': 0, 'C': 0}, google_responses[:1])
 
