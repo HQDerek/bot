7a4a63f35f1ca72d4a980f387e86fb9d37a6ca09
Zachary <hello@zach.ie>
2018-07-13 11:58:18 +0100

    Caching (#12)
    * Add cache refresh and prune commands
    
    * Add support for wikipedia caching and test_games initial file
    
    * Update query cache and split into separate files
    
    * Fix method one
    
    * Add skipping of unresolved wikipedia links
    
    * Add count of cached pages
    
    * Update cache commands to work with single database
    
    * Update cache import and export commands
    
    * Add db dumps

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -19,16 +19,11 @@ def question_api_response():
 def test_find_answer_words_google(question_api_response):  # pylint: disable=redefined-outer-name
     """ testing basic behaviour in find_answer_words_google """
 
-    mock_result = Mock()
-    mock_result.url = "/"
-    mock_result.text = "Example Response"
-
-    mock_session = Mock()
-    mock_session.get.return_value.result.return_value = mock_result
+    mock_response = Mock()
+    mock_response.url = "/"
+    mock_response.text = "Example Response"
 
     question, answers = question_api_response
-    google_responses = utils.build_google_queries(question, answers, mock_session)
-
-    confidence = utils.find_answer_words_google(question, answers, {'A': 0, 'B': 0, 'C': 0}, google_responses[:1])
+    confidence = utils.find_answer_words_google(question, answers, {'A': 0, 'B': 0, 'C': 0}, [mock_response])
 
     assert confidence == {'A': 0, 'B': 0, 'C': 0}
