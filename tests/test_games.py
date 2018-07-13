204b0ad468082d2008d4f36c84d3082c1e102b10
Zachary <hello@zach.ie>
2018-07-13 20:53:51 +0100

    Fix cache checking and result count missing

--- tests/test_games.py
+++ tests/test_games.py
@@ -3,6 +3,7 @@ from glob import glob
 from json import load
 from mock import Mock, patch
 from requests_cache import CachedSession
+import utils
 
 def cached_get(url):
     """ cached session """
@@ -16,16 +17,20 @@ def test_games(_mock_futures):
     """ testing games """
     total_correct = 0
     total_questions = 0
+    original_correct = 0
     for filename in glob('games/*.json'):
         game = load(open(filename))
         correct = 0
         for turn in game.get('questions'):
             turn['is_replay'] = True
-            # (prediction, _confidence) = utils.predict_answers(turn, turn.get('answers'))
-            # if prediction == turn.get('correct'):
-            #     correct += 1
+            with patch('builtins.print'):
+                (prediction, _confidence) = utils.predict_answers(turn, turn.get('answers'))
+            if prediction == turn.get('correct'):
+                correct += 1
             total_questions += 1
         total_correct += correct
+        original_correct += game.get('numCorrect')
         print('Game %s: %s/%s correct (Original: %s)' % \
             (game.get('showId'), correct, len(game.get('questions')), game.get('numCorrect')))
-    print('Total correct: %s/%s' % (total_correct, total_questions))
+    print('Total correct: %s/%s (Original: %s)' % \
+        (total_correct, total_questions, original_correct))
