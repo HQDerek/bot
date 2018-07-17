41189c4e47610f1c8c9d79a204ae53a605d3b8ec
Zachary <hello@zach.ie>
2018-07-17 10:11:06 +0100

    Fix replay command to work with single game

--- tests/test_games.py
+++ tests/test_games.py
@@ -1,37 +1,31 @@
 """ unit tests for the hqtrivia_bot.py """
 from glob import glob
 from json import load
-from mock import Mock, patch
-from requests_cache import CachedSession
+from mock import patch
 import utils
 
-def cached_get(url):
-    """ cached session """
-    session = CachedSession('db/cache', allowable_codes=(200, 302, 304))
-    response = Mock()
-    response.result.return_value = session.get(url)
-    return response
 
-@patch('utils.FuturesSession.get', side_effect=cached_get)
-def test_games(_mock_futures):
+def test_games():
     """ testing games """
     correct = {}
     num_questions = {}
     orig_correct = {}
     for filename in sorted(glob('games/*.json')):
         game = load(open(filename))
+        count_correct = 0
         for turn in game.get('questions'):
             turn['is_replay'] = True
             number = turn.get('questionNumber')
             with patch('builtins.print'):
                 (prediction, _confidence) = utils.predict_answers(turn, turn.get('answers'))
+            count_correct += (1 if prediction == turn.get('correct') else 0)
             correct[number] = correct.get(number, 0) + \
                 (1 if prediction == turn.get('correct') else 0)
             orig_correct[number] = orig_correct.get(number, 0) + \
                 (1 if turn.get('correct') == turn.get('prediction').get('answer') else 0)
             num_questions[number] = num_questions.get(number, 0) + 1
-        print('Game %s: correct: %s' % \
-            (game.get('showId'), correct))
+        print('Game %d: %d correct: %s' % \
+            (game.get('showId'), count_correct, correct))
     count_correct = sum(correct.values())
     count_orig_correct = sum(orig_correct.values())
     count_num_questions = sum(num_questions.values())
