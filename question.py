bc3633bab7790a84178d1697ffa3d9aa78748917
Dean <deansherwin@gmail.com>
2018-07-18 20:47:19 +0100

    question saving

--- question.py
+++ question.py
@@ -5,7 +5,7 @@ from json import load, dump
 class Question(object):
     """ An instance of a HQ Trivia question """
 
-    def __init__(self, is_replay=False, **kwargs):
+    def __init__(self, is_replay=False, load_id=None, **kwargs):
         self.id = kwargs.get('questionId', None)
         self.number = kwargs.get('questionNumber', None)
         self.text = kwargs.get('question', None)
@@ -13,27 +13,37 @@ class Question(object):
         self.correct = kwargs.get("correct", None)
         self.category = kwargs.get("category", None)
         self.prediction = kwargs.get("prediction", {})
+        self.correct = kwargs.get("correct", None)
         self.is_replay = is_replay
 
-    def get_output_path(self):
+        if load_id is not None:
+            output_key = -1 if self.is_replay else 'questions'
+            with open(self.game_path) as file:
+                output = load(file)
+            questions = output[output_key]
+            kwarg_vals = next((q for q in questions if q["questionId"] == load_id), None)
+            self.__init__(is_replay=is_replay, **kwarg_vals)
+
+    @property
+    def game_path(self):
         """ Determine what file to save question in """
         path = 'replay_results.json'
 
         if self.is_replay is False:
-            game_files = glob.glob('games/*.json')
+            game_files = glob('games/*.json')
             path = max(game_files, key=os.path.getctime)
 
         return path
+
 
     def save(self):
         """
         Checks most recently created results file, checks it for question with same id.
         If present, updates. If not, appends itself.
         """
-        file_path = self.get_output_path()
         output_key = -1 if self.is_replay else 'questions' # last list in replay games or 'questions' key in live saved games
 
-        with open(file_path) as file:
+        with open(self.game_path) as file:
             output = load(file)
 
         # pull questions out of file
@@ -41,9 +51,10 @@ class Question(object):
 
         updated = False
         for idx, saved_question in enumerate(questions):
-            if saved_question['id'] == self.id:
+            if saved_question['questionId'] == self.id:
                 questions[idx] = self._dict_for_json()
                 updated = True
+                break
         if updated is False:
             questions.append(self._dict_for_json())
 
@@ -51,7 +62,7 @@ class Question(object):
         output[output_key] = questions
 
         # Update save game file
-        with open(file_path, 'w') as file:
+        with open(self.game_path, 'w') as file:
             dump(output, file, ensure_ascii=False, sort_keys=True, indent=4)
 
     def display_summary(self):
@@ -65,12 +76,16 @@ class Question(object):
         }
         self.save()
 
-    def add_correct(self):
-        pass
+    def add_correct(self, correct):
+        """ Add correct answer to question and save """
+        self.correct = correct
+        self.save()
 
     def _dict_for_json(self):
         """ Convert instance to dict with correct keys in preparation for saving to JSON """
-        output = vars(self)
+        output = vars(self).copy()
         output['questionId'] = output.pop('id', None)
         output['questionNumber'] = output.pop('number', None)
+        output['question'] = output.pop('text', None)
+        output.pop('is_replay')
         return output
