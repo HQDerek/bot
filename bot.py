9d40e80eba619fc63bddc39e3cd95707cab55f29
Zachary <hello@zach.ie>
2018-08-29 19:29:03 +0100

    Add better command line argument parsing and split caching

--- bot.py
+++ bot.py
@@ -3,13 +3,12 @@ import webbrowser
 from os import path
 from glob import glob
 from time import sleep
-from sqlite3 import connect
 from datetime import datetime
 from configparser import ConfigParser
-from json import load, loads, dump, JSONDecodeError
+from json import loads, dump, JSONDecodeError
 from pytz import utc
 from dateutil import parser
-from requests import get, post, Request
+from requests import get, post
 from requests_cache import CachedSession
 from requests_futures.sessions import FuturesSession
 from websocket import WebSocketApp, WebSocketException, WebSocketTimeoutException
@@ -80,7 +79,7 @@ class HqTriviaBot(object):
                 config = ConfigParser()
                 config.read(filename)
                 other_headers = headers.copy()
-                other_headers.update({'Authorization': 'Bearer %s' %  config.get('Auth', 'bearer_token')})
+                other_headers.update({'Authorization': 'Bearer %s' % config.get('Auth', 'bearer_token')})
                 print("Making it rain for %s:" % (filename.split('-')[1] if len(filename.split('-')) > 1 else 'me'))
                 self.make_it_rain(other_headers)
         except TypeError:
@@ -118,6 +117,7 @@ class HqTriviaBot(object):
 
         # Run solvers
         responses = {}
+        prediction = None
         confidence = {'A': 0, 'B': 0, 'C': 0}
         for solver in self.solvers:
             responses[solver] = solver.fetch_responses(
@@ -142,11 +142,10 @@ class HqTriviaBot(object):
                                                  answer_key,
                                                  question.answers.get(answer_key),
                                                  confidence[answer_key])
-            print(Colours.OKBLUE.value + Colours.BOLD.value + result + Colours.ENDC.value \
-                if answer_key == prediction else result)
+            print(Colours.OKBLUE.value + Colours.BOLD.value + result + Colours.ENDC.value
+                  if answer_key == prediction else result)
 
         return prediction
-
 
     @staticmethod
     def game_summary(data):
@@ -184,7 +183,7 @@ class HqTriviaBot(object):
                     correct_index = next((n for (n, val)
                                           in enumerate(data.get('answerCounts'))
                                           if val["correct"]))
-                    correct_choice = chr(65 + correct_index) # A, B or C
+                    correct_choice = chr(65 + correct_index)  # A, B or C
                     question = Question(is_replay=False, load_id=data.get('questionId'))
                     question.add_correct(correct_choice)
                     question.display_summary()
@@ -203,7 +202,7 @@ class HqTriviaBot(object):
                 print('ERROR - bad json: %s' % message)
 
     def run(self):
-        """ functional loop(s) """
+        """ Run the bot with a live game websocket """
         if not self.config.has_section('Auth'):
             exit('Error: Config file \'config.ini\' with [Auth] section not found. Please run generate-token.')
         while True:
@@ -233,25 +232,22 @@ class HqTriviaBot(object):
                     print('\nSleeping until {} ({} seconds)'.format(next_show_time.strftime('%c'), seconds_until_show))
                     sleep(seconds_until_show)
 
-    def generate_token(self, phone):
-        """
-        generate a JWT for a particular phone
-        phone should be in a format such as '+353861234567'
-        """
+    def generate_token(self, number):
+        """ Generate an auth token for number """
         unauth_headers = self.headers.copy()
         unauth_headers.pop('Authorization', None)
         phone_resp = post('https://api-quiz.hype.space/verifications', headers=unauth_headers, data={
             'method': 'sms',
-            'phone': phone
+            'phone': number
         }).json()
         verification_id = phone_resp.get('verificationId')
         if not verification_id:
             print('Something went wrong. %s' % phone_resp.get('error', ''))
         else:
-            print('Verification sent to %s.' % phone)
+            print('Verification sent to %s.' % number)
             code = input("Please enter the code: ")
-            code_resp = post('https://api-quiz.hype.space/verifications/%s' % verification_id, \
-                headers=unauth_headers, data={'code': code}).json()
+            code_resp = post('https://api-quiz.hype.space/verifications/%s' % verification_id,
+                             headers=unauth_headers, data={'code': code}).json()
             if not code_resp.get('auth'):
                 print('Something went wrong. %s' % code_resp.get('error', ''))
             else:
@@ -264,135 +260,25 @@ class HqTriviaBot(object):
                     ))
                 print('Verification successful. Details stored in %s' % verify_file)
 
-    def get_wins(self, username):
-        """ get the amount of times a specific user has won """
-        resp = get('https://api-quiz.hype.space/users?q={"username":"%s"}' % username, headers=self.headers)
+    def get_stats(self, username):
+        """ Query play stats for a given user """
+        resp = get(f'https://api-quiz.hype.space/users?q={username}', headers=self.headers)
         try:
             json = resp.json()
+            user_id = None
             users = json.get('data', [])
             if users is not None:
                 for user in users:
                     if user.get('username') == username:
                         user_id = user.get('userId')
-                        resp = get('https://api-quiz.hype.space/users/%s' % user_id, headers=self.headers)
-                        user = resp.json()
-                        print('User:\t\t%s' % user.get('username'))
-                        print('Total Earnings:\t%s' % user.get('leaderboard').get('total'))
-                        print('Games Played:\t%s' % user.get('gamesPlayed'))
-                        print('Wins:\t\t%s' % user.get('winCount'))
+                        user = get('https://api-quiz.hype.space/users/{}'.format(user_id), headers=self.headers).json()
+                        print('User:\t\t{}'.format(user.get('username')))
+                        print('Total Earnings:\t{}'.format(user.get('leaderboard').get('total')))
+                        print('Games Played:\t{}'.format(user.get('gamesPlayed')))
+                        print('Wins:\t\t{}'.format(user.get('winCount')))
+                if not user_id:
+                    print('%s is not a user.' % username)
             else:
                 print('%s is not a user.' % username)
         except JSONDecodeError:
             pass
-
-
-
-    def cache(self, command):
-        """ cache mode """
-        session = CachedSession('db/cache', allowable_codes=(200, 302, 304))
-        solvers = [
-            GoogleAnswerWordsSolver(),
-            GoogleResultsCountSolver()
-        ]
-        print('Running cache %s' % command)
-        if command == 'prune':
-            self.cache_prune(session, solvers)
-        elif command == 'refresh':
-            self.cache_refresh(session, solvers)
-        elif command == 'vacuum':
-            self.cache_vacuum(session, solvers)
-        elif command == 'import':
-            self.cache_import(session, solvers)
-        elif command == 'export':
-            self.cache_export(session, solvers)
-
-    @staticmethod
-    def cache_prune(session, solvers):
-        """ cache prune mode """
-        urls = []
-        for solver in solvers:
-            for filename in sorted(glob('games/*.json')):
-                game = load(open(filename))
-                for turn in game.get('questions'):
-                    urls.extend(solver.build_urls(turn.get('question'), turn.get('answers')))
-        stale_entries = []
-        for key, (resp, _) in session.cache.responses.items():
-            if resp.url not in urls and not any(step.url in urls for step in resp.history):
-                stale_entries.append((key, resp))
-        print('Found %s/%s stale entries' % (len(stale_entries), len(session.cache.responses.keys())))
-        for key, resp in stale_entries:
-            print('Deleting stale entry: %s' % resp.url)
-            session.cache.delete(key)
-
-    @staticmethod
-    def cache_refresh(session, solvers):
-        """ cache refresh mode """
-        urls = []
-        for solver in solvers:
-            for filename in sorted(glob('games/*.json')):
-                game = load(open(filename))
-                for turn in game.get('questions'):
-                    urls.extend(solver.build_urls(turn.get('question'), turn.get('answers')))
-        cache_misses = [
-            url for url in urls if not session.cache.create_key(
-                session.prepare_request(Request('GET', url))
-            ) in session.cache.responses
-        ]
-        print('Found %s/%s URLs not in cache' % (len(cache_misses), len(urls)))
-        for idx, url in enumerate(cache_misses):
-            print('Adding cached entry: %s' % url)
-            response = session.get(url)
-            if '/sorry/index?continue=' in response.url:
-                exit('ERROR: Google rate limiting detected. Cached %s pages.' % idx)
-
-    @staticmethod
-    def cache_vacuum(_session, _solvers):
-        """ cache vacuum mode """
-        conn = connect("db/cache.sqlite")
-        conn.execute("VACUUM")
-        conn.close()
-
-    @staticmethod
-    def cache_import(_session, _solvers):
-        """ cache import mode """
-        conn = connect("db/cache.sqlite")
-        for filename in sorted(glob('db/*.sql')):
-            print('Importing SQL %s' % filename)
-            sql = open(filename, 'r').read()
-            cur = conn.cursor()
-            cur.executescript(sql)
-        conn.close()
-
-    @staticmethod
-    def cache_export(session, solvers):
-        """ cache export mode """
-        for filename in sorted(glob('games/*.json')):
-            game = load(open(filename))
-            show_id = path.basename(filename).split('.')[0]
-            if not path.isfile('./db/%s.sql' % show_id):
-                print('Exporting SQL %s' % show_id)
-                urls = []
-                for solver in solvers:
-                    for turn in game.get('questions'):
-                        urls.extend(solver.build_urls(turn.get('question'), turn.get('answers')))
-                url_keys = [session.cache.create_key(session.prepare_request(Request('GET', url))) for url in urls]
-                conn = connect(':memory:')
-                cur = conn.cursor()
-                cur.execute("attach database 'db/cache.sqlite' as cache")
-                cur.execute("select sql from cache.sqlite_master where type='table' and name='urls'")
-                cur.execute(cur.fetchone()[0])
-                cur.execute("select sql from cache.sqlite_master where type='table' and name='responses'")
-                cur.execute(cur.fetchone()[0])
-                for key in list(set(url_keys)):
-                    cur.execute("insert into urls select * from cache.urls where key = '%s'" % key)
-                    cur.execute("insert into responses select * from cache.responses where key = '%s'" % key)
-                conn.commit()
-                cur.execute("detach database cache")
-                with open('db/%s.sql' % show_id, 'w') as file:
-                    for line in conn.iterdump():
-                        file.write('%s\n' % line.replace(
-                            'TABLE', 'TABLE IF NOT EXISTS'
-                        ).replace(
-                            'INSERT', 'INSERT OR IGNORE'
-                        ))
-                conn.close()
--- main.py
+++ main.py
@@ -1,27 +1,90 @@
 #!/usr/bin/python
 """ Main module """
+import argparse
 from sys import argv
 from bot import HqTriviaBot
-from replay import Replayer
+import replay
+import cache
 
-if __name__ == "__main__":
-    BOT = HqTriviaBot()
-    if len(argv) == 2 and argv[1] == "run":
-        BOT.run()
-    elif len(argv) == 3 and argv[1] == "cache":
-        BOT.cache(argv[2])
-    elif len(argv) >= 2 and argv[1] == "replay":
-        REPLAYER = Replayer()
-        REPLAYER.play()
-        REPLAYER.gen_report()
-    elif len(argv) == 3 and argv[1] == "get-wins":
-        BOT.get_wins(argv[2])
-    elif len(argv) == 3 and argv[1] == "generate-token":
-        BOT.generate_token(argv[2])
-    else:
-        print('Error: Invalid syntax. Valid commands:')
-        print('hqtrivia-bot.py run')
-        print('hqtrivia-bot.py get-wins <username>')
-        print('hqtrivia-bot.py generate-token <phone>')
-        print('hqtrivia-bot.py replay <game-id>[,<game-id>]')
-        print('hqtrivia-bot.py cache <refresh|prune|vacuum>')
+
+class Main(object):
+    """ Main class for defining command line arguments """
+
+    def __init__(self):
+        self.bot = HqTriviaBot()
+        self.parser = argparse.ArgumentParser(
+            usage=f'''pipenv run <command> [<args>]
+
+Valid commands are:
+   bot       {self.bot.run.__doc__}
+   cache     {cache.__doc__}
+   replay    {replay.__doc__}
+   stats     {self.bot.get_stats.__doc__}
+   token     {self.bot.generate_token.__doc__}''',
+            prog='pipenv run'
+        )
+        self.parser.add_argument('command', help=argparse.SUPPRESS)
+        args = self.parser.parse_args(argv[1:2])
+        if not hasattr(self, args.command):
+            self.parser.print_help()
+            exit(1)
+
+        getattr(self, args.command)()
+
+    def run(self):
+        """ Run the bot against the live game """
+        parser = argparse.ArgumentParser(description=self.bot.run.__doc__,
+                                         prog=f'{self.parser.prog} bot')
+        parser.add_argument('--test-server', action='store_true')
+        args = vars(parser.parse_args(argv[2:]))
+        if args.get('test_server', '') is True:
+            self.bot.api_url = 'http://localhost:8765'
+        self.bot.run()
+
+    def cache(self):
+        """ Perform caching operations """
+        cacher = cache.Cache()
+        parser = argparse.ArgumentParser(
+            prog=f'{self.parser.prog} cache',
+            usage=f'''pipenv run cache <operation>
+
+Valid cache commands are:
+   prune       {cacher.prune.__doc__}
+   refresh     {cacher.refresh.__doc__}
+   vacuum      {cacher.vacuum.__doc__}
+   import_sql  {cacher.import_sql.__doc__}
+   export      {cacher.export.__doc__}
+''')
+        parser.add_argument('operation', help=argparse.SUPPRESS)
+        args = parser.parse_args(argv[2:3])
+        if not hasattr(cacher, args.operation):
+            parser.print_help()
+            exit(1)
+        getattr(cacher, args.operation)()
+
+    @staticmethod
+    def replay():
+        """ Replay a game and generate report """
+        replayer = replay.Replayer()
+        replayer.play()
+        replayer.gen_report()
+
+    def stats(self):
+        """ Query play stats for a given user """
+        parser = argparse.ArgumentParser(description=self.bot.get_stats.__doc__,
+                                         prog=f'{self.parser.prog} stats')
+        parser.add_argument('username', help="Username of the user to query")
+        args = vars(parser.parse_args(argv[2:]))
+        self.bot.get_stats(**args)
+
+    def token(self):
+        """ Generate an auth token for number """
+        parser = argparse.ArgumentParser(description=self.bot.generate_token.__doc__,
+                                         prog=f'{self.parser.prog} token')
+        parser.add_argument('number', help='Number in international format e.g. +353861230000')
+        args = vars(parser.parse_args(argv[2:]))
+        self.bot.generate_token(**args)
+
+
+if __name__ == '__main__':
+    Main()
